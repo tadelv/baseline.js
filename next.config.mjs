79756d7afe42a56c07caafacdae1851067044b7d
Vid Tadel <tadelv@gmail.com>
2026-02-04 22:36:00 +0100

    initial commig

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
+
+
